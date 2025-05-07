4b07752f65ff626b9d3b1d3f06b056cc2a79563a
P Naveen Chandra <pnaveen_chandra@trimble.com>
2025-05-07 17:51:21 +0530

    adding console class updates

---
+++ console-class/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace console_class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.DarkGreen;
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine("calling a beep sound");
+            Console.Beep();
+            
+        }
+    }
+}
