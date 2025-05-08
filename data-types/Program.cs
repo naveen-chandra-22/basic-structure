e4742ff2a9c2a6b9bee88f10ce1ed21a7392867f
naveen-chandra-22 <pnaveen_chandra@trimble.com>
2025-05-08 13:15:44 +0530

    data types and pointers

---
+++ data-types/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace data_types
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string variable = "string varaible print H";
+            foreach (var newItem in variable.ToArray())
+            {
+
+                Console.WriteLine($"i am writing in interpolation: {(SqlInt64)((variable.Length * sizeof(Char))) / 10}");
+            }
+            SqlInt32 sqlvalues = new SqlInt32(16);
+            //if (sqlvalues.IsNull)
+            //{
+            //    Console.WriteLine(sqlvalues.Value);
+
+            //}
+
+
+            // pointer type
+            //unsafe
+            //{
+            //    int i = 0;
+            //    int* j = &i;
+
+            //    Console.WriteLine($"write value: {i}");
+            //    Console.WriteLine($"write address: {(int)j}");
+            //}
+
+        }
+    }
+}
